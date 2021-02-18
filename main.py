dae307032b2773570306d06baff9e12876052951
Daniel Soares <67944213+asoushawk@users.noreply.github.com>
2021-02-18 03:00:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import csv 
+import os
 from datetime import datetime
+
 
 
 URL = "https://en.wikipedia.org/wiki/COVID-19_vaccine"
@@ -18,17 +20,19 @@ trs = body.find_all('tr')
 rows = trs[2].find_all('td')
 
 
-#print("Monde: ", row[0].a.string)
 
 day = datetime.today().strftime('%Y-%m-%d')
 
-#for row in rows:
+
+
+os.remove('database.csv')
 
 countries = []
 
 index = 2
-with open('database.csv', 'a', newline='') as csvfile:
+with open('database.csv', 'w', newline='') as csvfile:
 	writer = csv.writer(csvfile)
+	writer.writerow(['day', 'country', 'vaccinated', 'percentage"'])
 	writer.writerow("\n")
 
 
@@ -63,46 +67,59 @@ body = vaxx_dist.tbody
 trs = body.find_all('tr')
 index = 2
 
-
-with open('cases.csv', 'a', newline='') as csvfile:
+with open ('output.csv', 'a', newline='') as csvfile:
 	writer = csv.writer(csvfile)
+	writer.writerow('\n')
 
-with open('output.csv', 'a', newline='') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow("\n")	
 
 	
-
-for tr in trs:
-	td = trs[index].find_all('td')
-	th = trs[index].find_all('th')
-	deaths = td[0].string
-	cases = td[1].string
-	country = th[1].a.string
-	print(country, deaths, cases)
-	index = index + 1
-	with open('cases.csv' ,'a', newline='') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow([day, country, cases, deaths])
-
-	for a in countries:
-		with open('cases.csv', 'a', newline='') as csvfile:
+try:
+	for tr in trs:
+		td = trs[index].find_all('td')
+		th = trs[index].find_all('th')
+		deaths = td[0].string
+		cases = td[1].string
+		country = th[1].a.string
+		#print(country, deaths, cases)
+		index = index + 1
+		with open('cases.csv' ,'a', newline='') as csvfile:
 			writer = csv.writer(csvfile)
+			writer.writerow([day, country, cases, deaths])
+		for a in countries:
 			if country == a:
 				with open('database.csv', 'r', newline='') as csvfile:
 					reader = csv.DictReader(csvfile)
-					reader1 = csv.reader(csvfile)
 					for row in reader:
 						if row['country'] == country:
 							vaccinated = row['vaccinated']
-							percentage = row['percentage']
+							percentage = row['percentage"']
+							with open("nha.csv") as csvfile:
+								reader = csv.DictReader(csvfile)
+								for row1 in reader:
+									nhacountry = row1['Countries']
+									if nhacountry == 'United States of America':
+										nhacountry = 'United States'	
+									if a == nhacountry:
+										expenditure = row1['2018']
+										with open("economyrank.csv") as csvfile1:
+											reader = csv.DictReader(csvfile1)
+											for row2 in reader:
+												if row2['country'] == country:
+													economyrank = row2['economyrank']
+													economyrankposition = row2['economyrankposition']
+													fout = [day, a, deaths, cases, vaccinated, percentage, expenditure, economyrank, economyrankposition]
+													with open("output.csv", "a+") as fhandle:
+														writer = csv.writer(fhandle)
+														writer.writerow(fout)
 
-							fout = [day, a, deaths, cases, vaccinated, percentage]
-							print(vaccinated)
-							with open("output.csv", "a+") as fhandle:
-								writer = csv.writer(fhandle)
-								writer.writerow(fout)
-								break
+except:
+	pass
+
+
+										
+									
+
+							
 
 
 						
